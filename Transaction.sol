e7630940293595f04df22432e3c5f937243656a9
Kadam <111586677+Kadam-Anita@users.noreply.github.com>
2025-06-14 10:18:28 +0530

    Create Transaction.sol

---
+++ Transaction.sol
@@ -0,0 +1,11 @@
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.0;
+contract Pay{
+address payable user payable (0xAb8483F640
+function payEther() public payable { }
+function getBalance() public view returns (unit){
+return address(this).balance ; }
+function sendEtherAccount() public 
+{ user.transfer(1 wei); }
+}
